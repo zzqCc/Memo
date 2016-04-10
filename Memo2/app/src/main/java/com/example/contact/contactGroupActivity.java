2951ca80732c0fe6f24eff098edac3803b20306a
cc <609979753@qq.com>
2016-04-10 18:17:08 +0800

    add contact database

--- Memo2/app/src/main/java/com/example/contact/contactGroupActivity.java
+++ Memo2/app/src/main/java/com/example/contact/contactGroupActivity.java
@@ -1,7 +1,38 @@
 package com.example.contact;
 
+import android.app.Activity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import com.example.R;
+import com.example.database.MemoDatabaseHelper;
+
 /**
  * Created by lx on 2016/4/9.
  */
-public class contactGroupActivity {
+public class contactGroupActivity extends Activity {
+    private MemoDatabaseHelper dbHelper;
+    private SQLiteDatabase db;
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_group);
+        dbHelper = new MemoDatabaseHelper(this,"Memo_main.db",null,1);
+        db = dbHelper.getWritableDatabase();
+
+        String sql = "SELECT * FROM group ";
+        Cursor cursor = db.rawQuery(sql,null);
+        ListView listView = (ListView) findViewById(R.id.contact_people_listview);
+        ContactGroupCursorAdapter contactCursorAdapter = new ContactGroupCursorAdapter(this,cursor,0);//flags
+        listView.setAdapter(contactCursorAdapter);
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        if( db!=null && db.isOpen() )
+            db.close();
+    }
+
 }
--- Memo2/app/src/main/java/com/example/database/MemoDatabaseHelper.java
+++ Memo2/app/src/main/java/com/example/database/MemoDatabaseHelper.java
@@ -24,7 +24,11 @@ public class MemoDatabaseHelper extends SQLiteOpenHelper {
             "contact_id INTEGER," +
             "name TEXT);";
     public static final String CREATE_RELATION = "CREATE TABLE relation(" + //!public!
-            "people_id INTEGER PRIMARY KEY AUTOINCREMENT);";
+            "_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "person_name TEXT);" ; //邻接矩阵
+    public static final String CREATE_GROUP = "CREATE TABLE group(" +
+            "_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "group_name TEXT);"; // store group name
 
 
 
@@ -35,9 +39,71 @@ public class MemoDatabaseHelper extends SQLiteOpenHelper {
     }
     @Override
     public void onCreate(SQLiteDatabase db){
+        //-----------------------------------------------------------------------------
+        //for test
+        // memo test
+        String sqltest = "insert into memo (title,content," +
+                "d_day,send_day) values(?,?,datetime('now','localtime'),?);";
+        db.execSQL(sqltest,new String[] {"hello","hello acmer","datetime('now','+1 hour','localtime')"});
+        db.execSQL(sqltest,new String[] {"goodbye","goodbye acmer","datetime('now','-1 hour','localtime')"});
+        db.execSQL(sqltest,new String[] {"good luck","good luck falling acmer","datetime('now','+1 day','localtime')"});
+        db.execSQL(sqltest,new String[] {"touch over there","falling in this  acmer","datetime('now','+1 year','localtime')"});
+
+        // insert contact, the number after the name is to specify the group they belong to ,
+        // for example, boby138 belong to group1,group3 and group8
+        sqltest = "INSERT INTO contact (contact_id,name) VALUES (312435432,\"boby138\");";
+        db.execSQL(sqltest,null);
+        sqltest = "INSERT INTO contact (contact_id,name) VALUES (312432,\"tomi145\");";
+        db.execSQL(sqltest, null);
+        sqltest = "INSERT INTO contact (contact_id,name) VALUES (31432,\"anna126\");";
+        db.execSQL(sqltest,null);
+        sqltest = "INSERT INTO contact (contact_id,name) VALUES (31243,\"andy456\");";
+        db.execSQL(sqltest,null);
+        sqltest = "INSERT INTO contact (contact_id,name) VALUES (32,\"john234\");";
+        db.execSQL(sqltest,null);
+
+        String sqltest2; // add group, it should be done in both RELATIONSHIP and GROUP table
+        sqltest = "INSERT INTO group (group_name) VALUES (\"group1\")";
+        sqltest2 = "ALTER TABLE relation ADD group1 BOOLEAN DEFAULT 0;";
+        db.execSQL(sqltest,null); db.execSQL(sqltest2,null);
+        sqltest = "INSERT INTO group (group_name) VALUES (\"group2\")";
+        sqltest2 = "ALTER TABLE relation ADD group2 BOOLEAN DEFAULT 0;";
+        db.execSQL(sqltest,null);db.execSQL(sqltest2,null);
+        sqltest = "INSERT INTO group (group_name) VALUES (\"group3\")";
+        sqltest2 = "ALTER TABLE relation ADD group3 BOOLEAN DEFAULT 0;";
+        db.execSQL(sqltest,null);db.execSQL(sqltest2,null);
+        sqltest = "INSERT INTO group (group_name) VALUES (\"group4\")";
+        sqltest2 = "ALTER TABLE relation ADD group4 BOOLEAN DEFAULT 0;";
+        db.execSQL(sqltest,null);db.execSQL(sqltest2,null);
+        sqltest = "INSERT INTO group (group_name) VALUES (\"group5\")";
+        sqltest2 = "ALTER TABLE relation ADD group5 BOOLEAN DEFAULT 0;";
+        db.execSQL(sqltest,null);db.execSQL(sqltest2,null);
+        sqltest = "INSERT INTO group (group_name) VALUES (\"group6\")";
+        sqltest2 = "ALTER TABLE relation ADD group6 BOOLEAN DEFAULT 0;";
+        db.execSQL(sqltest,null);db.execSQL(sqltest2,null);
+        sqltest = "INSERT INTO group (group_name) VALUES (\"group7\")";
+        sqltest2 = "ALTER TABLE relation ADD group7 BOOLEAN DEFAULT 0;";
+        db.execSQL(sqltest,null);db.execSQL(sqltest2,null);
+        sqltest = "INSERT INTO group (group_name) VALUES (\"group8\")";
+        sqltest2 = "ALTER TABLE relation ADD group8 BOOLEAN DEFAULT 0;";
+        db.execSQL(sqltest,null);db.execSQL(sqltest2,null);
+        sqltest = "INSERT INTO group (group_name) VALUES (\"group9\")";
+        sqltest2 = "ALTER TABLE relation ADD group9 BOOLEAN DEFAULT 0;";
+        db.execSQL(sqltest,null);db.execSQL(sqltest2,null);
+
+        //classify persons in group
+        sqltest = "INSERT INTO relation (person_name,?,?,?) VALUES (?,1,1,1);";
+        db.execSQL(sqltest,new String[] {"group1","group3","group8","boby138"});
+        db.execSQL(sqltest,new String[] {"group1","group4","group5","tomi145"});
+        db.execSQL(sqltest,new String[] {"group1","group2","group6","anna126"});
+        db.execSQL(sqltest,new String[] {"group4","group5","group6","andy456"});
+        db.execSQL(sqltest,new String[] {"group2","group3","group4","john234"});
+        //---------------------------------------------------------------
+        //-----------------------------------------------------------------------------
         db.execSQL(CREATE_MEMO);
         db.execSQL(CREATE_CONTACT);
         db.execSQL(CREATE_RELATION);
+        db.execSQL(CREATE_GROUP);
     }
 
     @Override
