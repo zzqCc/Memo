a1470c69f728583b638d3649ebaaebb01851476f
cc <609979753@qq.com>
2016-04-10 10:03:14 +0800

    add datepicker

--- EActivity2/app/src/main/java/com/example/administrator/eactivity/choosetime.java
+++ EActivity2/app/src/main/java/com/example/administrator/eactivity/choosetime.java
@@ -1,5 +1,6 @@
 package com.example.administrator.eactivity;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 import android.app.Activity;
@@ -12,8 +13,7 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TimePicker;
 import android.widget.DatePicker.OnDateChangedListener;
-import android.widget.TimePicker.OnTimeChangedListener;
-
+import android.widget.Toast;
 
 
 public class choosetime extends Activity {
@@ -28,18 +28,32 @@ public class choosetime extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choosetime);
         timeEt=(EditText)findViewById(R.id.timeEt);
+        dateEt=(EditText)findViewById(R.id.datEt);
         TimePicker timePicker=(TimePicker)findViewById(R.id.timePicker);
         Yeba = (Button) findViewById(R.id.yeah);
+        DatePicker datePicker = (DatePicker) findViewById(R.id.dpPicker);
 
         Calendar calendar=Calendar.getInstance();
         int year=calendar.get(Calendar.YEAR);
         int monthOfYear=calendar.get(Calendar.MONTH);
         int dayOfMonth=calendar.get(Calendar.DAY_OF_MONTH);
+        datePicker.init(year, monthOfYear, dayOfMonth, new OnDateChangedListener(){
+
+            public void onDateChanged(DatePicker view, int year,
+                                      int monthOfYear, int dayOfMonth) {
+                dateEt.setText("您选择的日期是：" + year + "年" + (monthOfYear + 1) + "月" + dayOfMonth + "日。");
+            }
+
+        });
 
 
-        timePicker.setOnTimeChangedListener(new OnTimeChangedListener() {
+        timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
 
+            @Override
             public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+                Toast.makeText(choosetime.this,
+                        hourOfDay + "小时" + minute + "分钟",
+                        Toast.LENGTH_SHORT).show();
                 timeEt.setText("您选择的时间是：" + hourOfDay + "时" + minute + "分。");
             }
 
@@ -50,8 +64,8 @@ public class choosetime extends Activity {
             @Override
             public void onClick(View v) {
                 Intent backWithTime = new Intent();
-                backWithTime.putExtra("data_return" , timeEt.getText());
-                setResult(RESULT_OK, backWithTime);
+                backWithTime.putExtra("data_return" , dateEt.getText().toString()+timeEt.getText().toString());
+                setResult(RESULT_OK,backWithTime);
                 finish();
             }
         });
