6cfbc7d424ece7e4402776fdc95af4432272bae2
QichenHuang <1049385379@qq.com>
2016-04-11 13:25:01 +0800

    cancel option item picture

---
+++ Memo2/app/src/main/java/com/example/lx/memo/OptionItem.java
@@ -0,0 +1,16 @@
+package com.example.lx.memo;
+
+/**
+ * Created by 启琛 on 2016/4/11.
+ */
+public class OptionItem
+{
+    public OptionItem(int id,String item)
+    {
+        ImageId=id;
+        itemname=item;
+    }
+    public int ImageId;
+    public String itemname;
+
+}
