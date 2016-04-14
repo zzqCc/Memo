c8996519baadd35a274cbd28c997bf069f5e0d04
QichenHuang <1049385379@qq.com>
2016-04-14 12:49:21 +0800

    加入登录界面框架“ git commit -m加入登陆界面框架

--- Memo2/app/src/main/java/com/example/lx/memo/LogIn.java
+++ Memo2/app/src/main/java/com/example/lx/memo/LogIn.java
@@ -22,8 +22,15 @@ public class LogIn extends Activity
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent =new Intent(LogIn.this,MainActivity.class);
+                Intent intent = new Intent(LogIn.this, MainActivity.class);
                 startActivity(intent);
+            }
+        });
+        TextView textView1=(TextView)findViewById(R.id.register);
+        textView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LogIn.this,"立即注册",Toast.LENGTH_SHORT).show();
             }
         });
         TextView textView=(TextView)findViewById(R.id.find_password);
